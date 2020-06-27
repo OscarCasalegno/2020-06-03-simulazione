4b8f0e95247c50cfd6a75ef7a0016ae38cb3dc62
OscarAlberto <s245739@studenti.polito.it>
2020-06-27 12:20:23 +0200

    punto 1.a, 1.b, 1.c

--- src/main/java/it/polito/tdp/PremierLeague/FXMLController.java
+++ src/main/java/it/polito/tdp/PremierLeague/FXMLController.java
@@ -15,60 +15,74 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class FXMLController {
-	
+
 	private Model model;
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
-    private ResourceBundle resources;
+	@FXML // ResourceBundle that was given to the FXMLLoader
+	private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
-    private URL location;
+	@FXML // URL location of the FXML file that was given to the FXMLLoader
+	private URL location;
 
-    @FXML // fx:id="btnCreaGrafo"
-    private Button btnCreaGrafo; // Value injected by FXMLLoader
+	@FXML // fx:id="btnCreaGrafo"
+	private Button btnCreaGrafo; // Value injected by FXMLLoader
 
-    @FXML // fx:id="btnTopPlayer"
-    private Button btnTopPlayer; // Value injected by FXMLLoader
+	@FXML // fx:id="btnTopPlayer"
+	private Button btnTopPlayer; // Value injected by FXMLLoader
 
-    @FXML // fx:id="btnDreamTeam"
-    private Button btnDreamTeam; // Value injected by FXMLLoader
+	@FXML // fx:id="btnDreamTeam"
+	private Button btnDreamTeam; // Value injected by FXMLLoader
 
-    @FXML // fx:id="txtK"
-    private TextField txtK; // Value injected by FXMLLoader
+	@FXML // fx:id="txtK"
+	private TextField txtK; // Value injected by FXMLLoader
 
-    @FXML // fx:id="txtGoals"
-    private TextField txtGoals; // Value injected by FXMLLoader
+	@FXML // fx:id="txtGoals"
+	private TextField txtGoals; // Value injected by FXMLLoader
 
-    @FXML // fx:id="txtResult"
-    private TextArea txtResult; // Value injected by FXMLLoader
+	@FXML // fx:id="txtResult"
+	private TextArea txtResult; // Value injected by FXMLLoader
 
-    @FXML
-    void doCreaGrafo(ActionEvent event) {
+	@FXML
+	void doCreaGrafo(ActionEvent event) {
+		this.txtResult.clear();
+		Double goals;
+		try {
+			goals = Double.parseDouble(this.txtGoals.getText());
+		} catch (NumberFormatException e) {
+			this.txtResult.appendText("Inserisci un numero di goal valido");
+			return;
+		}
+		if (goals < 0) {
+			this.txtResult.appendText("Inserire un numero maggiore di 0");
+			return;
+		}
 
-    }
+		this.model.creaGrafo(goals);
 
-    @FXML
-    void doDreamTeam(ActionEvent event) {
+	}
 
-    }
+	@FXML
+	void doDreamTeam(ActionEvent event) {
 
-    @FXML
-    void doTopPlayer(ActionEvent event) {
+	}
 
-    }
+	@FXML
+	void doTopPlayer(ActionEvent event) {
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-        assert btnCreaGrafo != null : "fx:id=\"btnCreaGrafo\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert btnTopPlayer != null : "fx:id=\"btnTopPlayer\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert btnDreamTeam != null : "fx:id=\"btnDreamTeam\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert txtK != null : "fx:id=\"txtK\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert txtGoals != null : "fx:id=\"txtGoals\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+	}
 
-    }
-    
-    public void setModel(Model model) {
-    	this.model = model;
-    }
+	@FXML // This method is called by the FXMLLoader when initialization is complete
+	void initialize() {
+		assert btnCreaGrafo != null : "fx:id=\"btnCreaGrafo\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert btnTopPlayer != null : "fx:id=\"btnTopPlayer\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert btnDreamTeam != null : "fx:id=\"btnDreamTeam\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert txtK != null : "fx:id=\"txtK\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert txtGoals != null : "fx:id=\"txtGoals\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+
+	}
+
+	public void setModel(Model model) {
+		this.model = model;
+	}
 }
--- src/main/java/it/polito/tdp/PremierLeague/model/Model.java
+++ src/main/java/it/polito/tdp/PremierLeague/model/Model.java
@@ -1,5 +1,55 @@
 package it.polito.tdp.PremierLeague.model;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleDirectedWeightedGraph;
+
+import it.polito.tdp.PremierLeague.db.Conn;
+import it.polito.tdp.PremierLeague.db.PremierLeagueDAO;
+
 public class Model {
+
+	Graph<Player, DefaultWeightedEdge> graph;
+	Map<Integer, Player> idPlayers;
+
+	public void creaGrafo(Double goals) {
+		this.graph = new SimpleDirectedWeightedGraph<>(DefaultWeightedEdge.class);
+
+		List<Player> players = PremierLeagueDAO.listPlayersByAvgGoals(goals);
+
+		this.idPlayers = new HashMap<>();
+		for (Player p : players) {
+			this.idPlayers.put(p.getPlayerID(), p);
+		}
+
+		Graphs.addAllVertices(this.graph, players);
+
+		List<Conn> con = PremierLeagueDAO.getConn();
+
+		for (Conn c : con) {
+
+			if (this.graph.containsVertex(this.idPlayers.get(c.getId1()))
+					&& this.graph.containsVertex(this.idPlayers.get(c.getId2()))) {
+				if (c.getDiff() < 0) {
+					DefaultWeightedEdge e = this.graph.addEdge(this.idPlayers.get(c.getId2()),
+							this.idPlayers.get(c.getId1()));
+					this.graph.setEdgeWeight(e, -(c.getDiff().doubleValue()));
+				} else if (c.getDiff() > 0) {
+					DefaultWeightedEdge e = this.graph.addEdge(this.idPlayers.get(c.getId1()),
+							this.idPlayers.get(c.getId2()));
+					this.graph.setEdgeWeight(e, c.getDiff().doubleValue());
+				}
+			}
+
+		}
+
+		System.out.println(this.graph.vertexSet());
+		System.out.println(this.graph.edgeSet());
+	}
 
 }
